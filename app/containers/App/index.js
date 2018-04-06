0296e03ca89ff5795dc83c794edfc96171a569a3
Katie Fedoseeva <katiefedoseeva77@gmail.com>
2018-04-05 21:58:14 -0700

    adding stuff

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -23,6 +23,7 @@ import AdminPosts from 'containers/AdminPosts/Loadable';
 import StudentSignedInLandingPage from 'containers/StudentSignedInLandingPage/Loadable';
 import ArchivedPosts from 'containers/ArchivedPosts/Loadable';
 import AdminInbox from 'containers/AdminInbox/Loadable';
+import AdminFeed from 'containers/AmindInbox/Loadable';
 import TutorSignedInLandingPage from 'containers/TutorSignedInLandingPage/Loadable';
 /**import containers end*/
 
@@ -83,6 +84,7 @@ export default function App() {
     <Route path="/StudentSignedInLandingPage" component={StudentSignedInLandingPage} />
     <Route path="/ArchivedPosts" component={ArchivedPosts} />
     <Route path="/AdminInbox" component={AdminInbox} />
+    <Route path="/AmindFeed" component={AdminFeed} />
     <Route path="/AdminPosts" component={AdminPosts} />
     <Route path="/TutorSignedInLandingPage" component={TutorSignedInLandingPage} />
         <Route path="" component={NotFoundPage} />
