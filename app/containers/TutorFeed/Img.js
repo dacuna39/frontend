ff18378913e9672f696d39d3cb976f5e1049175c
Diego Acuna <dacuna39@yahoo.com>
2018-04-20 14:57:36 -0700

    post owner info loads with posts now

--- app/containers/TutorFeed/Img.js
+++ app/containers/TutorFeed/Img.js
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 import NormalImg from 'components/Img';
 
 const Img = styled(NormalImg)`
-  width: 50px;
-  height: 50px:
+width: 50px;
+height: 50px;
 `;
 
 export default Img;
